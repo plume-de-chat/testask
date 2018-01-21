ab7615bc5662913c8a1ff8adea9dbb20a9e53bc6
plume-de-chat <35663728+plume-de-chat@users.noreply.github.com>
2018-01-21 18:39:27 +0100

    ajout cron
    ajouté cron

--- TASK.h
+++ TASK.h
@@ -1,6 +1,7 @@
 
 #ifndef DaKTASK
 #define DaKTASK
+
 // la tâche est effectuee avec un delai de PeriodeMilliSecondes entre deux executions. 
 // consomme 4 octets de RAM
 #define Task(NomTache,PeriodeMilliSecondes)  for( \
@@ -16,8 +17,19 @@
     (millis_ ## NomTache = millis()) - DaKTASK_ ## NomTache >= (unsigned long)(PeriodeMilliSecondes); \
     DaKTASK_ ## NomTache = millis_ ## NomTache \
     )
+	
+// la tâche est effectuee avec un delai de PeriodeMilliSecondes entre deux executions. 
+// consomme 4 octets de RAM
+#define cron(PeriodeMilliSecondes)  for( \
+    static unsigned long __nextmillis = 0; \
+    millis() - __nextmillis >= (unsigned long)(PeriodeMilliSecondes); \
+    __nextmillis = millis() \
+    )
+
 
 #endif
+
+
 
 /*
 
@@ -35,6 +47,8 @@ void loop() {
   Task( line, 3000) Serial.println(F("-----------------"));
   
   FTask( LED, 100 ){ PORTB ^= 0x20; }  // clignoter la LED  
+  
+  cron(500){ PORTB ^= 0x20; }  // clignoter la LED toutes les secondes
   
   delay(11);
 }
